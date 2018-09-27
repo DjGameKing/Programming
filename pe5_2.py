f226fa093813eda44bb82ac4c92e20473ea7bf1a
DESKTOP-RHKIU7P\damia <damiandeemsjordan@hotmail.com>
2018-09-27 16:38:40 +0200

    He

---
+++ pe5_2.py
@@ -0,0 +1,7 @@
+leeftijd = int(input('Geef je leeftijd: '))
+nlID = input('Nederlands paspoort: ')
+
+if leeftijd < 18 or nlID == 'no' or nlID == 'nee' or nlID == 'Nee' or nlID == 'No':
+    print('Helaas, u mag niet stemmen!')
+else:
+    print('Gefeliciteerd, u mag stemmen!')
